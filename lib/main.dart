a79f1a9d8156ea48134958bf299fa8c3f9ca8d3b
Hardik Srivastava <hardiksrivastav.vns@gmail.com>
2024-01-01 21:06:11 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'dart:developer';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:webrtc_tutorial/routes/app_route_config.dart';
 import 'package:webrtc_tutorial/signaling.dart';
-
+import 'package:webrtc_tutorial/test.dart';
+import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(MyApp());
 }
 
@@ -13,19 +19,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      routeInformationParser: GoRouterConfig.router.routeInformationParser,
+      routerDelegate: GoRouterConfig.router.routerDelegate,
+      routeInformationProvider: GoRouterConfig.router.routeInformationProvider,
+      
+      debugShowCheckedModeBanner: false
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
-
+  MyHomePage({Key? key, required this.id}) : super(key: key);
+  String id;
+  
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -43,12 +54,26 @@ class _MyHomePageState extends State<MyHomePage> {
     _remoteRenderer.initialize();
 
     signaling.onAddRemoteStream = ((stream) {
+      
       _remoteRenderer.srcObject = stream;
       setState(() {});
     });
-
+    
     super.initState();
+    textEditingController.text=widget.id;
+    roomId=widget.id;
+    Future.delayed(Duration.zero).then((value) {
+       signaling.joinRoom(
+                      textEditingController.text.trim(),
+                      _remoteRenderer,
+                    );
+    }).then((value) => setState(() {
+                      print("heklo");
+                    },));
+    
   }
+
+  
 
   @override
   void dispose() {
@@ -59,85 +84,90 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    print("room Id is");
+    print(widget.id);
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Welcome to Flutter Explained - WebRTC"),
-      ),
+      // appBar: AppBar(
+      //   title: Text("Welcome to Flutter Explained - WebRTC"),
+      // ),
       body: Column(
         children: [
-          SizedBox(height: 8),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  signaling.openUserMedia(_localRenderer, _remoteRenderer);
-                },
-                child: Text("Open camera & microphone"),
-              ),
-              SizedBox(
-                width: 8,
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  roomId = await signaling.createRoom(_remoteRenderer);
-                  textEditingController.text = roomId!;
-                  setState(() {});
-                },
-                child: Text("Create room"),
-              ),
-              SizedBox(
-                width: 8,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  // Add roomId
-                  signaling.joinRoom(
-                    textEditingController.text.trim(),
-                    _remoteRenderer,
-                  );
-                },
-                child: Text("Join room"),
-              ),
-              SizedBox(
-                width: 8,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  signaling.hangUp(_localRenderer);
-                },
-                child: Text("Hangup"),
-              )
-            ],
-          ),
-          SizedBox(height: 8),
+          // SizedBox(height: 8),
+          // SingleChildScrollView(
+          //   scrollDirection: Axis.horizontal,
+          //   child: Row(
+          //     mainAxisAlignment: MainAxisAlignment.center,
+          //     children: [
+          //       // ElevatedButton(
+          //       //   onPressed: () {
+          //       //     signaling.openUserMedia(_localRenderer, _remoteRenderer);
+          //       //   },
+          //       //   child: Text("Open camera & microphone"),
+          //       // ),
+          //       // SizedBox(
+          //       //   width: 8,
+          //       // ),
+          //       // ElevatedButton(
+          //       //   onPressed: () async {
+          //       //     roomId = await signaling.createRoom(_remoteRenderer);
+          //       //     textEditingController.text = roomId!;
+          //       //     setState(() {});
+          //       //   },
+          //       //   child: Text("Create room"),
+          //       // ),
+          //       // SizedBox(
+          //       //   width: 8,
+          //       // ),
+          //       ElevatedButton(
+          //         onPressed: () {
+          //           // Add roomId
+          //           signaling.joinRoom(
+          //             textEditingController.text.trim(),
+          //             _remoteRenderer,
+          //           );
+          //         },
+          //         child: Text("Join room"),
+          //       ),
+          //       SizedBox(
+          //         width: 8,
+          //       ),
+          //       ElevatedButton(
+          //         onPressed: () {
+          //           signaling.hangUp(_localRenderer);
+          //         },
+          //         child: Text("Hangup"),
+          //       )
+          //     ],
+          //   ),
+          // ),
+          // SizedBox(height: 8),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Expanded(child: RTCVideoView(_localRenderer, mirror: true)),
+                  // Expanded(child: RTCVideoView(_localRenderer, mirror: true)),
                   Expanded(child: RTCVideoView(_remoteRenderer)),
                 ],
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Join the following Room: "),
-                Flexible(
-                  child: TextFormField(
-                    controller: textEditingController,
-                  ),
-                )
-              ],
-            ),
-          ),
-          SizedBox(height: 8)
+          // Padding(
+          //   padding: const EdgeInsets.all(8.0),
+          //   child: Row(
+          //     mainAxisAlignment: MainAxisAlignment.center,
+          //     children: [
+          //       Text("Join the following Room: "),
+          //       Flexible(
+          //         child: TextFormField(
+          //           controller: textEditingController,
+          //         ),
+          //       )
+          //     ],
+          //   ),
+          // ),
+          // SizedBox(height: 8)
         ],
       ),
     );
